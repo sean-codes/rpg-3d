cda0b064ba1017af508e11c79499519d63691757
Sean Codes <sean_codes@outlook.com>
2020-05-23 00:20:02 -0500

    texture / light experimenting

--- notes/adding_a_light.md
+++ notes/adding_a_light.md
@@ -3,6 +3,7 @@ To make our shape not look so flat we will add a light!
 
 Before we can add a light and make it work we will need to change out MeshBasicMaterial into one that can be affected by light. We can use `MeshPhongMaterial`, `MeshStandardMaterial`, or `MeshPhysicalMaterial`. For this we will use Phong since it seems to be the best for performance.
 
+# Experiment
 Change Basic Material for Phong
 ```js
 const material = new THREE.MeshPhongMaterial({ color: "#465" });
@@ -30,3 +31,24 @@ const light = new THREE.DirectionalLight(0xffffff, 1);
 light.position.set(2, 2, 4);
 scene.add(light);
 ```
+
+## Light types
+There a couple useful types of lights
+- `AmbientLight` - Lights everything
+- `HemisphereLight` - Lights everything top to bottom
+- `DirectionalLight` - Points towards a target
+- `PointLight` - Like a lightbulb
+
+## Helpers
+Those two lights can attach a helper
+
+```js
+// PointLightHelper
+const lightHelper = new THREE.PointLightHelper(light, 1, '#F22');
+
+// DirectionalLightHelper
+const lightHelper = new THREE.DirectionalLightHelper(light, 1, '#F22');
+```
+
+## Directional Light Target
+A Directional Light uses a target which is an object that can be added to the scene and moved around. You can also assign any object that has a `.position` property on it!
--- src/index.js
+++ src/index.js
@@ -1,47 +1,104 @@
 // Not sure yet how we will structure this :]
 // Lets figure out the basics of using three first
+const gui = new dat.GUI();
+
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+window.onresize = resize;
 
+// scene
 const scene = new THREE.Scene();
+
+// camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-camera.position.z = 5;
+camera.position.set(0, 5, 5);
+// camera.lookAt(0, 0, 0);
+const controls = new THREE.OrbitControls(camera, renderer.domElement);
+controls.target.set(0, 1, 0);
+controls.update();
 
 
-// lets render a cube!
-const geometry = new THREE.BoxGeometry();
-const material1 = new THREE.MeshPhongMaterial({ color: "#465" });
-const material2 = new THREE.MeshPhongMaterial({ color: "#F22" });
-const material3 = new THREE.MeshPhongMaterial({ color: "#666" });
-const cube1 = new THREE.Mesh(geometry, material1);
-const cube2 = new THREE.Mesh(geometry, material2);
-cube2.position.set(2, 0, 0);
-const cube3 = new THREE.Mesh(geometry, material3);
-cube3.position.set(-2, 0, 0);
-scene.add(cube1);
-scene.add(cube2);
-scene.add(cube3);
+// a plane with a texture
+const planeSize = 20;
 
-// Add a light
-const light = new THREE.DirectionalLight("#FFF", 1);
-light.position.set(2, 2, 4);
+const loader = new THREE.TextureLoader();
+const texture = loader.load('./src/assets/checker.png');
+texture.wrapS = THREE.RepeatWrapping;
+texture.wrapT = THREE.RepeatWrapping;
+texture.magFilter = THREE.NearestFilter;
+const repeats = planeSize;
+texture.repeat.set(repeats, repeats);
+
+const planeGeometry = new THREE.PlaneBufferGeometry(planeSize, planeSize);
+const planeMaterial = new THREE.MeshPhongMaterial({ map: texture, side: THREE.DoubleSide });
+const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
+planeMesh.rotation.x = Math.PI * -0.5;
+scene.add(planeMesh);
+
+// a circle
+const sunGeometry = new THREE.SphereBufferGeometry();
+const sunMaterial = new THREE.MeshPhongMaterial({ color: '#a2f', flatShading: true })
+const sunMesh = new THREE.Mesh(sunGeometry, sunMaterial);
+sunMesh.position.y += 1.5;
+scene.add(sunMesh);
+
+// Ambient light
+const light = new THREE.PointLight("#FFF", 1);
+const lightHelper = new THREE.PointLightHelper(light, 1, '#F22');
+scene.add(lightHelper);
+light.position.y += 4;
+// light.target.position.set(-5, 0, 0);
 scene.add(light);
+// scene.add(light.target);
 
+function updateLight() {
+  lightHelper.update();
+}
+
+// const targetFolder = gui.addFolder('target')
+// targetFolder.add(light.target.position, 'x', -10, 10).onChange(updateLight);
+// targetFolder.add(light.target.position, 'y', -10, 10).onChange(updateLight);
+// targetFolder.add(light.target.position, 'z', -10, 10).onChange(updateLight);
+// targetFolder.add(light, 'intensity', 0, 1);
+// targetFolder.open();
+
+const lightFolder = gui.addFolder('light')
+lightFolder.add(light.position, 'x', -10, 10).onChange(updateLight);
+lightFolder.add(light.position, 'y', -10, 10).onChange(updateLight);
+lightFolder.add(light.position, 'z', -10, 10).onChange(updateLight);
+lightFolder.add(light, 'distance', 1, 50).onChange(updateLight);
+lightFolder.add(light, 'intensity', 0, 1);
+lightFolder.open();
+
+
+
+// light
+// const light = new THREE.DirectionalLight();
+// light.position.z += 2
+// scene.add(light);
+
+
+function resize() {
+  console.log('resizing');
+  const pixelRatio = window.devicePixelRatio
+  const width = window.innerWidth * pixelRatio
+  const height = window.innerHeight * pixelRatio
+  renderer.domElement.width = width;
+  renderer.domElement.height = height;
+  renderer.setSize(width, height, false);
+  camera.aspect = width/height;
+  camera.updateProjectionMatrix();
+  // lightHelper.update();
+}
 
 function render() {
   requestAnimationFrame(render);
   renderer.render(scene, camera);
 
-
-  cube1.rotation.x += 0.01;
-  cube1.rotation.y += 0.01;
-
-  cube2.rotation.x += 0.01;
-  cube2.rotation.y += 0.01;
-
-  cube3.rotation.x += 0.01;
-  cube3.rotation.y += 0.01;
+  sunMesh.rotation.x -= 0.01;
+  sunMesh.rotation.y -= 0.01;
 }
 
+
+resize();
 render();
