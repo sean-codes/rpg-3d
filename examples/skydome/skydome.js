0c2cd3dc9fcbf564673e0642a634754c0f1c8aa0
Sean Codes <sean_codes@outlook.com>
2020-11-11 02:19:18 -0600

    🌓 starCount to skydome

--- examples/skydome/skydome.js
+++ examples/skydome/skydome.js
@@ -1,36 +1,66 @@
 // init
 const scene = new THREE.Scene()
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000)
 const renderer = new THREE.WebGLRenderer()
 document.body.appendChild(renderer.domElement)
 const controls = new THREE.OrbitControls(camera, renderer.domElement)
+camera.position.z = 0.0000001
 
-camera.position.z = 0.0001
+// settings
+var settings = {
+  skyColor: "#aa9ed7",
+  refColor: "#0f0b18",
+  canvasSize: 2048,
+  starCount: 10000,
+  size: 20,
+  transition: 0.4,
+}
 
-const skyColor = "#FFF"
-// const refColor = "#41b9ee"
-const refColor = "#7F00FF"
-const size = 50
+const datGui = new dat.GUI()
+datGui.add(settings, 'size', 0, 2048, 1).onChange(updateSky);
+datGui.add(settings, 'transition', 0, 1, 0.05).onChange(updateSky);
+datGui.add(settings, 'starCount', 0, 50000, 10).onChange(updateSky);
+datGui.addColor(settings, 'skyColor').onChange(updateSky);
+datGui.addColor(settings, 'refColor').onChange(updateSky);
 
-// Sides
+// texture
 const skyCanvas = document.createElement('canvas')
 const skyCtx = skyCanvas.getContext('2d')
-skyCanvas.width = size
-skyCanvas.height = size
-skyCtx.fillStyle = skyColor
-skyCtx.fillRect(0, 0, skyCanvas.width, skyCanvas.height)
-var grd = skyCtx.createLinearGradient(0, 0, 0, skyCanvas.height);
-grd.addColorStop(0, refColor);
-grd.addColorStop(0.75, skyColor);
-grd.addColorStop(0.9, skyColor);
-skyCtx.fillStyle = grd;
-skyCtx.fillRect(0, 0, skyCanvas.width, skyCanvas.height)
+const skyTexture = new THREE.CanvasTexture(skyCanvas)
 
-
-const geo = new THREE.SphereGeometry(20, 60, 40)
-const mat = new THREE.MeshBasicMaterial({ map: new THREE.CanvasTexture(skyCanvas), side: THREE.BackSide })
+// add mesh
+const geo = new THREE.SphereGeometry(50)
+const mat = new THREE.MeshBasicMaterial({ map: skyTexture, side: THREE.BackSide })
 const mes = new THREE.Mesh(geo, mat)
 scene.add(mes)
+
+// on change
+function updateSky() {
+   skyCanvas.width = settings.canvasSize
+   skyCanvas.height = settings.canvasSize
+   skyCtx.fillStyle = settings.skyColor
+   skyCtx.fillRect(0, 0, skyCanvas.width, skyCanvas.height)
+   var grd = skyCtx.createLinearGradient(0, 0, 0, skyCanvas.height);
+   grd.addColorStop(0, settings.refColor);
+   grd.addColorStop(settings.transition, settings.refColor);
+   grd.addColorStop(1, settings.skyColor);
+   skyCtx.fillStyle = grd;
+   skyCtx.fillRect(0, 0, skyCanvas.width, skyCanvas.height)
+   
+   for (var i = 0; i < settings.starCount; i++) {
+      skyCtx.fillStyle = `rgba(255, 255, 255, ${0.25 + Math.random()*0.75})`;
+      const x = Math.random()*skyCanvas.width
+      const y = Math.random()*skyCanvas.height/2 + Math.random()*skyCanvas.height*0.25
+      const size = Math.random()*2
+      skyCtx.fillRect(x, y, size, size)
+   }
+   
+   skyTexture.needsUpdate = true
+}
+
+updateSky()
+
+
 
 
 // render
